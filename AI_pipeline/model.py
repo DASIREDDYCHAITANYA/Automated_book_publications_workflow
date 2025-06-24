e2082bb98c25ae9e62bee5889494a5205b660855
Chaitanya <chaitanya@Chaitanyas-MacBook-Air-2.local>
2025-06-24 12:13:57 +0530

    first commit

--- AI_pipeline/model.py
+++ AI_pipeline/model.py
@@ -2,6 +2,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 
 model_name = "microsoft/phi-2"
-tokenizer = AutoTokenizer.from_pretrained(model_name, token="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")
-model = AutoModelForCausalLM.from_pretrained(model_name, token="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")
+tokenizer = AutoTokenizer.from_pretrained(model_name, token="******")
+model = AutoModelForCausalLM.from_pretrained(model_name, token="****")
 generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
